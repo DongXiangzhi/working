cf3ae3bd61b037fb99e6d20ae13b9a62bbba9004
DongXiangzhi <upsunny2008@163.com>
2018-04-24 09:24:27 +0800

    修改bug，Page_Spider可以调用Utilities

--- Page_Spider.py
+++ Page_Spider.py
@@ -1,14 +1,22 @@
-import os
+# import os
 import argparse
+from utilities import url_utilities
 
 
 def main(database: str, url_list_file: str):
-    print('我们将使用数据库：' + database);
-    print('我们将抓取以下页面：' + url_list_file);
+    big_word_list = []
+    print('我们将使用数据库：' + database)
+    print('我们将抓取以下页面：' + url_list_file)
+    urls = url_utilities.load_urls_from_file(url_list_file)
+    for url in urls:
+        print('当前：' + url)
+        page_content = url_utilities.load_page(url)
+        words = url_utilities.scrape_page(page_content)
+        big_word_list.extend(words)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser();
+    parser = argparse.ArgumentParser()
     parser.add_argument("-db", "--database", help="SQLite文件名称")
     parser.add_argument("-i", "--input", help="包含待抓取页面URL的文件")
     args = parser.parse_args()
--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 def load_urls_from_file(file_path: str):
     try:
         with open(file_path) as f:
-            content = f.readline()
+            content = f.readlines()
             return content
     except FileNotFoundError:
         print('文件:' + file_path + '不存在！')
